b6a5861e51c5c121a145672b3ff09e6eae465062
Daniel Andrés Segura <danielcordero@Daniels-MacBook-Pro-2.fritz.box>
2023-04-30 20:16:20 +0200

    feat: 3 requiered props to scaffold class

--- lib/main.dart
+++ lib/main.dart
@@ -2,24 +2,32 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(const HomePageClass());
 
-class HomePageClass extends StatelessWidget{
+class HomePageClass extends StatelessWidget {
   const HomePageClass({super.key});
-@override
-Widget build(BuildContext context) {
-  return const MaterialApp(
-    home: StatefulClassProvider(),
-  );
-}}
 
-class StatefulClassProvider extends StatefulWidget{
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: StatefulClassProvider(),
+    );
+  }
+}
+
+class StatefulClassProvider extends StatefulWidget {
   const StatefulClassProvider({super.key});
+
   @override
   StreamClass createState() => StreamClass();
 }
 
-class StreamClass extends State<StatefulClassProvider>{
+class StreamClass extends State<StatefulClassProvider> {
   @override
-  Widget build(BuildContext context){
-    return const Scaffold();
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      appBar:,
+      body:,
+      bottomNavigationBar:,
+
+    );
   }
 }
